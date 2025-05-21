2ab7cd7a674c229f35df670aea646ea03c064547
lautySaez <lauty01saez@gmail.com>
2025-05-21 02:11:23 -0300

    Mis telas terminado, solo faltan las imagenes y cargar mis telas

--- src/main/java/com/tallerwebi/presentacion/controlador/ControladorTela.java
+++ src/main/java/com/tallerwebi/presentacion/controlador/ControladorTela.java
@@ -1,5 +1,6 @@
 package com.tallerwebi.presentacion.controlador;
 
+import com.tallerwebi.dominio.entidad.TipoTela;
 import com.tallerwebi.dominio.servicio.ServicioTela;
 import com.tallerwebi.presentacion.dto.MisTelas;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
   public class ControladorTela {
@@ -18,7 +21,8 @@ import java.util.List;
     @GetMapping("/catalogo-telas")
     public String mostrarCatalogoTelas(Model model) {
         List<MisTelas> telas = servicioTela.obtenerTelasDeFabrica();
-        model.addAttribute("telas", telas);
-        return "catalogo-telas"; // busca el HTML en templates/catalogo-telas.html
+        model.addAttribute("telas", telas); // esto es lo que el HTML espera
+        return "catalogo-telas";
     }
+
 }
